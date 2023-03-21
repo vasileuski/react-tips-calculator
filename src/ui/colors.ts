fddfbc1eb8e475820c724199f7256cf1c6201e13
Artsiom Vasileuski <vasileuski.artsiom@gmail.com>
2023-03-21 13:41:34 +0400

    init: start the project

---
+++ src/ui/colors.ts
@@ -0,0 +1,6 @@
+export enum Color {
+  Blue = "#74ADAC",
+  Gray = "#BEC2C3",
+  Black = "#0B0E10",
+  DarkGray = "#8A8E8E",
+}
---
+++ src/ui/media.ts
@@ -0,0 +1,5 @@
+export enum Media {
+  SMALL = `media screen and (max-width: 568px)`,
+  MEDIUM = `media screen and (max-width: 768px)`,
+  LARGE = `media screen and (max-width: 992px)`,
+}
